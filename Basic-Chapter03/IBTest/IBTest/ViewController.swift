72b5090c908860dac6c372b6e5236170fe50d24a
Mr. Remark <itsmine096@gmail.com>
2021-11-10 22:01:13 +0900

    Chapter 03. IBTest 프로젝트 실습코드 커밋

--- Basic-Chapter03/IBTest/IBTest/ViewController.swift
+++ Basic-Chapter03/IBTest/IBTest/ViewController.swift
@@ -13,7 +13,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBOutlet var uiTitle01: UILabel!
+    @IBOutlet var uiTitle02: UILabel!
+    @IBOutlet var uiTitle03: UILabel!
+    @IBOutlet var uiTitle04: UILabel!
+    @IBOutlet var uiTitle05: UILabel!
+    
+    @IBAction func clickBtn01(_ sender: Any) {
+        self.uiTitle01.text = "On"
+    }
+    @IBAction func clickBtn02(_ sender: Any) {
+        self.uiTitle02.text = "On"
+    }
+    @IBAction func clickBtn03(_ sender: Any) {
+        self.uiTitle03.text = "On"
+    }
+    @IBAction func clickBtn04(_ sender: Any) {
+        self.uiTitle04.text = "On"
+    }
+    @IBAction func clickBtn05(_ sender: Any) {
+        self.uiTitle05.text = "On"
+    }
+    
 
 }
 
