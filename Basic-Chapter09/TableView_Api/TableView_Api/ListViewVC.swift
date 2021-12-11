9f89a442495c11dbf7912c995d0942b5fa919bf3
Mr. Remark <itsmine096@gmail.com>
2021-12-11 20:29:28 +0900

    source code commit

--- Basic-Chapter09/TableView_Api/TableView_Api/ListViewVC.swift
+++ Basic-Chapter09/TableView_Api/TableView_Api/ListViewVC.swift
@@ -9,19 +9,20 @@ import UIKit
 
 class ListViewVC: UITableViewController {
     var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2009-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2008-10-08", 7.31),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19),
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2009-09-04", 8.95, "darknight.jpeg"),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2008-10-08", 7.31, "rain.jpeg"),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpeg"),
     ]
     
     lazy var list : [MovieVo] = {
         var datalist = [MovieVo]()
-        for (title, desc, opendate, rating) in self.dataset {
+        for (title, desc, opendate, rating, thumnail) in self.dataset {
             let mvo = MovieVo()
             mvo.title = title
             mvo.description = desc
             mvo.opendate = opendate
             mvo.rating = rating
+            mvo.thumnail = thumnail
             
             datalist.append(mvo)
         }
@@ -37,9 +38,13 @@ class ListViewVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = self.list[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
-        cell.textLabel?.text = row.title
+        cell.title?.text = row.title
+        cell.desc?.text = row.description
+        cell.opendate?.text = row.opendate
+        cell.rating?.text = "\(row.rating!)"
+        cell.img.image = UIImage(named: row.thumnail!)
         
         return cell
     }
