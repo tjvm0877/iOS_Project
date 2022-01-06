68153a8de91ed6353ae3a265a0f67d9ba441ad93
Mr. Remark <itsmine096@gmail.com>
2022-01-06 19:56:01 +0900

    Update ListViewVC.swift

--- Basic-Chapter09/TableView_Api/TableView_Api/ListViewVC.swift
+++ Basic-Chapter09/TableView_Api/TableView_Api/ListViewVC.swift
@@ -8,11 +8,6 @@
 import UIKit
 
 class ListViewVC: UITableViewController {
-//    var dataset = [
-//        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2009-09-04", 8.95, "darknight.jpeg"),
-//        ("호우시절", "때를 알고 내리는 좋은 비", "2008-10-08", 7.31, "rain.jpeg"),
-//        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpeg"),
-//    ]
 
     lazy var list : [MovieVo] = {
         var datalist = [MovieVo]()
