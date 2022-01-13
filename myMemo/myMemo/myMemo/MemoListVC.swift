49d657d99ec99cddcb107249612597017f59e5d7
Mr. Remark <itsmine096@gmail.com>
2022-01-13 18:48:56 +0900

    myMemo - 커스텀 클래스 생성(MemoListVC, MemoFormVC, MemoData), MemoFormVC구현

---
+++ myMemo/myMemo/myMemo/MemoListVC.swift
@@ -0,0 +1,15 @@
+//
+//  MemoListVC.swift
+//  myMemo
+//
+//  Created by Hyun on 2022/01/13.
+//
+
+import UIKit
+
+class MemoListVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
