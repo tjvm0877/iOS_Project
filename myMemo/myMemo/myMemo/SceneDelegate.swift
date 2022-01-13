9c8368c365c95c1d320233176e0778c9769279b5
Mr. Remark <itsmine096@gmail.com>
2022-01-13 15:07:58 +0900

    MyMemo 프로젝트 생성 (기존 ViewController 삭제)

--- myMemo/myMemo/myMemo/SceneDelegate.swift
+++ myMemo/myMemo/myMemo/SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  SceneDelegate.swift
-//  TableView
+//  myMemo
 //
-//  Created by Hyun on 2022/01/10.
+//  Created by Hyun on 2022/01/13.
 //
 
 import UIKit
@@ -45,6 +45,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
