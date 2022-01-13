ef62fdcac0274a7fd812ffad1c4a83fff9f9932b
Mr. Remark <itsmine096@gmail.com>
2022-01-13 15:06:29 +0900

    8, 9, 10, 11 디렉토리 이름 변경

---
+++ Basic-Chapter08, 09, 10, 11/TableView/TableView/MovieVO.swift
@@ -0,0 +1,20 @@
+//
+//  MovieVO.swift
+//  TableView
+//
+//  Created by Hyun on 2022/01/10.
+//
+
+import Foundation
+import UIKit
+
+class MovieVO {
+    var thumbnail: String?
+    var title : String?
+    var description : String?
+    var detail : String?
+    var openDate : String?
+    var rating : Double?
+    
+    var thumbnailImage: UIImage?
+}
